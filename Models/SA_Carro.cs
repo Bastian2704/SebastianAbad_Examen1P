688d35010389dae4d7a50e47ace561751d5bff0b
Bastian2704 <sebastianabad2704@gmail.com>
2024-10-28 09:49:35 -0500

    SA Creacion de Modelo Principal

---
+++ Models/SA_Carro.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SebastianAbad_Examen1P.Models
+{
+    [Table("SA_Tabla1")]
+    public class SA_Carro
+    {
+        [KeyAttribute]
+        public int SA_CARROID { get; set; }
+        [StringLength(1)]
+        public int SA_MecanicosAcargo { get; set; }
+        [MaxLength(30)]
+        [Required]
+        public string SA_Modelo { get; set; }
+        [MaxLength(20)]
+        [Required]
+        public string SA_Marca{ get; set; }
+        [Range(0, 5000)]
+        public decimal SA_Precio { get; set; }
+        public bool SA_Gasolina { get; set; }
+        public DateTime SA_FechaEntrega { get; set; }
+
+
+
+    }
+}
